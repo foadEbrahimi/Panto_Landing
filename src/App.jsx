b58a0baf6c0336e3c082de09bb94f13f288b30e1
Foad <fwad40901@gmail.com>
2024-09-19 13:57:52 +0330

    add react & tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import img from './assets/Rectangle 1441 (1).png';
+export default function App() {
+  return (
+    <div>
+      <img src={img} alt="" />
+    </div>
+  );
+}
