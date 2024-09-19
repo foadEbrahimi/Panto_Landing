7c5e38c0e42f8abd731acb6cec4dccb63dcfee3b
Foad <fwad40901@gmail.com>
2024-09-19 16:18:29 +0330

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gilroyBold: ['gilroyBold'],
+        gilroyHeavy: ['gilroyHeavy'],
+        gilroyLight: ['gilroyLight'],
+        gilroyMedium: ['gilroyMedium'],
+        gilroyRegular: ['gilroyRegular'],
+      },
+    },
   },
   plugins: [],
 };
