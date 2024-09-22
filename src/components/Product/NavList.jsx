698287f94a05c4b66cbcfbfc0334a8ba33d72aed
Foad <fwad40901@gmail.com>
2024-09-22 09:25:28 +0330

    animation hover in navListProduct

--- src/components/Product/NavList.jsx
+++ src/components/Product/NavList.jsx
@@ -6,11 +6,13 @@ export default function NavList() {
   const [list, setList] = useState('Chair');
   return (
     <div className="bg-[#eeeeee] px-2 py-1.5 rounded-full w-fit mx-auto mt-8">
-      <ul className="[&>*]:rounded-full [&>*]:lg:text-lg [&>*]:cursor-pointer [&>*]:px-4 [&>*]:py-2 flex gap-2 items-center justify-between">
+      <ul className="[&>*]:rounded-full [&>*]:lg:text-lg [&>*]:cursor-pointer [&>*]:px-4 [&>*]:py-2 flex gap-1 items-center justify-between">
         {NavListProduct.map(item => (
           <li
             key={item.id}
-            className={`${list === item.leble ? 'bg-white' : ''}`}
+            className={`${list === item.leble ? 'bg-white' : ''} z-10 relative ${
+              list !== item.leble && 'navList'
+            } overflow-hidden`}
             onClick={() => setList(item.leble)}
           >
             {item.leble}
