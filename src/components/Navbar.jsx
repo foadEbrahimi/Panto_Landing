e36a5293c066cb1875b093f7cdbd0a50c139172b
Foad <fwad40901@gmail.com>
2025-01-18 02:07:37 +0330

    add cart box

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import bag from '../assets/images/svgs/bag.svg';
 import list from '../assets/images/svgs/list.svg';
+import xBlack from '../assets/images/svgs/x-black.svg';
 import x from '../assets/images/svgs/x.svg';
 
 import Layout from '../Layout';
 
+import { useSelector } from 'react-redux';
 import useClickOutSide from '../hook/useClickOutSide';
+import { getTotalCartCount, getTotalCartPrice } from '../reducers/Cart';
 
 export default function Navbar() {
+  const [showCart, setShowCart] = useState(false);
   const [showList, setShowList] = useState(false);
+
   const close = () => setShowList(false);
   const { ref } = useClickOutSide(close);
+
+  const count = useSelector(getTotalCartCount);
+  const allPrice = useSelector(getTotalCartPrice);
+
   return (
     <Layout>
       <div
         ref={ref}
         id="navbar"
-        className="flex px-6 py-4 items-center justify-between"
+        className="flex px-6 py-4 relative items-center justify-between"
       >
         <div>
           <h1 className="text-2xl font-gilroyBold">Panto</h1>
@@ -49,11 +58,13 @@ export default function Navbar() {
         </div>
         {/* bag & list icon */}
         <div className="flex items-center gap-5 cursor-pointer">
-          <div className="relative">
+          <div className="relative" onClick={() => setShowCart(true)}>
             <img src={bag} className="w-6" alt="svg" />
-            <span className="rounded-full bg-[#e5be11] px-1.5 text-sm -right-2 text-white absolute -top-1">
-              5
-            </span>
+            {count > 0 && (
+              <span className="rounded-full bg-[#e5be11] px-1.5 text-sm -right-2 text-white absolute -top-1">
+                {count}
+              </span>
+            )}
           </div>
           <div className="md:hidden">
             <img
@@ -63,6 +74,19 @@ export default function Navbar() {
               className={`${showList ? 'w-5' : 'w-5'} cursor-pointer`}
             />
           </div>
+        </div>
+        {/* cart */}
+        <div
+          className={`absolute flex flex-col ${
+            showCart ? '' : 'translate-x-[100%]'
+          } p-3 w-[23rem] rounded-l-lg top-0 h-[100dvh] z-10 transition-all duration-500 bg-white -right-10`}
+        >
+          <img
+            src={xBlack}
+            onClick={() => setShowCart(false)}
+            className="w-5 cursor-pointer"
+            alt="xBlack svg"
+          />
         </div>
       </div>
     </Layout>
