f34acd40452dda39814349ce339a490b7c248628
Foad <fwad40901@gmail.com>
2025-01-18 13:20:41 +0330

    add products list

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,10 +4,29 @@ import img3 from '../assets/images/Testimonials/img3.png';
 import avatar1 from '../assets/images/Testimonials/avatar/avatar1.png';
 import avatar2 from '../assets/images/Testimonials/avatar/avatar2.png';
 import avatar3 from '../assets/images/Testimonials/avatar/avatar3.png';
-import pImg from '../assets/images/products/img1.png';
-import pImg2 from '../assets/images/products/img2.png';
-import pImg3 from '../assets/images/products/img3.png';
-import pImg4 from '../assets/images/products/img4.png';
+
+import chair1 from '../assets/images/products/Chair/chair1.png';
+import chair2 from '../assets/images/products/Chair/chair2.png';
+import chair3 from '../assets/images/products/Chair/chair3.png';
+import chair4 from '../assets/images/products/Chair/chair4.png';
+import chair5 from '../assets/images/products/Chair/chair5.png';
+import chair6 from '../assets/images/products/Chair/chair6.png';
+import chair7 from '../assets/images/products/Chair/chair7.png';
+import chair8 from '../assets/images/products/Chair/chair8.png';
+import chair9 from '../assets/images/products/Chair/chair9.png';
+import chair10 from '../assets/images/products/Chair/chair10.png';
+
+import bed1 from '../assets/images/products/Bed/bed1.png';
+import bed2 from '../assets/images/products/Bed/bed2.png';
+import bed3 from '../assets/images/products/Bed/bed3.png';
+
+import sofa1 from '../assets/images/products/Sofa/sofa1.png';
+import sofa2 from '../assets/images/products/Sofa/sofa3.png';
+import sofa3 from '../assets/images/products/Sofa/sofa4.png';
+
+import lamp1 from '../assets/images/products/Lamp/lamp1.png';
+import lamp2 from '../assets/images/products/Lamp/lamp2.png';
+import lamp3 from '../assets/images/products/Lamp/lamp3.png';
 
 export const Why = [
   {
@@ -90,32 +109,141 @@ export const Testimonials = [
 ];
 
 export const ProductsList = [
+  // Chair
   {
     id: 1,
     label: 'Sakarias Armchair',
     category: 'Chair',
     price: 392,
-    img: pImg,
+    img: chair1,
   },
   {
     id: 2,
     label: 'Baltsar Chair',
     category: 'Chair',
     price: 299,
-    img: pImg4,
+    img: chair2,
   },
   {
     id: 3,
     label: 'Anjay Chair',
     category: 'Chair',
     price: 519,
-    img: pImg2,
+    img: chair3,
   },
   {
     id: 4,
     label: 'Nyantuy Chair',
     category: 'Chair',
     price: 921,
-    img: pImg3,
+    img: chair4,
+  },
+  {
+    id: 5,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair5,
+  },
+  {
+    id: 6,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair6,
+  },
+  {
+    id: 7,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair7,
+  },
+  {
+    id: 8,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair8,
+  },
+  {
+    id: 9,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair9,
+  },
+  {
+    id: 10,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: chair10,
+  },
+  // Bed
+  {
+    id: 11,
+    label: 'Nyantuy Chair',
+    category: 'Beds',
+    price: 921,
+    img: bed1,
+  },
+  {
+    id: 12,
+    label: 'Nyantuy Chair',
+    category: 'Beds',
+    price: 921,
+    img: bed2,
+  },
+  {
+    id: 13,
+    label: 'Nyantuy Chair',
+    category: 'Beds',
+    price: 921,
+    img: bed3,
+  },
+  // Sofa
+  {
+    id: 15,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: sofa1,
+  },
+  {
+    id: 16,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: sofa2,
+  },
+  {
+    id: 17,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: sofa3,
+  },
+  // Lamp
+  {
+    id: 19,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: lamp1,
+  },
+  {
+    id: 20,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: lamp2,
+  },
+  {
+    id: 21,
+    label: 'Nyantuy Chair',
+    category: 'Chair',
+    price: 921,
+    img: lamp3,
   },
 ];
