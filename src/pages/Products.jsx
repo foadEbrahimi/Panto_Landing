a4c46f8ce14a810cebd928e9b4a58c5d5ad5ca2b
Foad <fwad40901@gmail.com>
2025-01-25 21:10:03 +0330

    create Products comp

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Products() {
   return (
     <div>
       all products
+      <Link to={`${2}`}>go to producs 2</Link>
     </div>
-  )
+  );
 }
